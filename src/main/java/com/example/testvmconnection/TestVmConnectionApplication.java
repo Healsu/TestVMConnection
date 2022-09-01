037fa0ca79a7f196afc50d526db02663e4eed481
Healsu <timmie2500@gmail.com>
2022-09-01 09:17:07 +0200

    Initial commit

---
+++ src/main/java/com/example/testvmconnection/TestVmConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.testvmconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVmConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestVmConnectionApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/testvmconnection/controller/Main.java
@@ -0,0 +1,7 @@
+package com.example.testvmconnection.controller;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
